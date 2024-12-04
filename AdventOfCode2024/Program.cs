4ec600970b7554e8a5e4156e4fda5a667d0c2174
James Philbrick <63430438+jamesphilbrick@users.noreply.github.com>
2024-12-04 21:40:44 +0000

    D2P2
    D2P1 rewrite and initial go at D2P2

--- AdventOfCode2024/Program.cs
+++ AdventOfCode2024/Program.cs
@@ -63,9 +63,14 @@ namespace AdventOfCode2024
             // ----- Part 1 -----
             List<string> levels = System.IO.File.ReadAllLines("..\\..\\Problem Input Files\\Input 2-1.txt", System.Text.Encoding.UTF8).ToList();
             int safeReportsCount = 0;
+            int safeReportsCountWithDampener = 0;
+
+            int test1ErrorCount = 0; // Increases whenever change from increasing to decreasing & visa versa
+            int test2ErrorCount = 0; // Increases whenever the adj. levels differ by <1 or >3
 
             foreach (string level in levels)
             {
+
                 // convert line to list of ints
                 List<int> values = level.Split(' ').Select(x => int.Parse(x)).ToList();
 
@@ -77,28 +82,29 @@ namespace AdventOfCode2024
                     differences.Add(pairDifference);
                 }
 
-                // check this difference list against the two conditions
-                if (differences.Where(x => (Math.Abs(x) > 3 || Math.Abs(x) == 0)).Count() > 0)
+                // get the number of +ve differences & number of -ve differences: smallest value is the number of times this test fails.
+                test1ErrorCount = Math.Min(differences.Count(x => x > 0), differences.Count(x => x < 0));
+                test2ErrorCount = differences.Where(x => (Math.Abs(x) > 3 || Math.Abs(x) == 0)).Count();
+                Console.WriteLine($"\nt1: {test1ErrorCount.ToString()}");
+                Console.WriteLine($"t2: {test2ErrorCount.ToString()}");
+
+                if (test1ErrorCount == 0 && test2ErrorCount == 0)
                 {
-                    // fail
-                    continue;
-                }
-                else if (differences.Count(x => x > 0) > 0 && 
-                         differences.Count(x => x < 0) > 0)
-                {
-                    // fail
-                    continue;
-                }
-                else
-                {
-                    // pass
                     safeReportsCount++;
                 }
+                else if (test1ErrorCount + test2ErrorCount < 2)
+                {
+                    safeReportsCountWithDampener++;
+                }
+                Console.WriteLine($"safe: {safeReportsCount.ToString()}");
+                Console.WriteLine($"safe w/ dampener: {(safeReportsCountWithDampener + safeReportsCount).ToString()}");
             }
-            Console.WriteLine(safeReportsCount.ToString());
+            
             Pause();
 
             // ----- Part 2 -----
+
+            // do the same checks as above, but increment a problem counter when a fail condition is met. If this counter <= 1, then the level is safe. 
         }
 
         static void Main(string[] args)
